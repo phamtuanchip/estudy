d0836d5e24434da1a6ea9ee97b5d47ce1dde4c09
Pham Tuan <tuanp@exoplatform.com>
2013-05-26 04:13:10 +0700

    add js module load and css

--- webui/src/main/webapp/javascript/estudy/estudy.js
+++ webui/src/main/webapp/javascript/estudy/estudy.js
@@ -1,6 +1,12 @@
-(function(base, gtnav, gj, uiForm, uiPopupWindow, wx){
+(function(captcha,base, gtnav, gj, uiForm, uiPopupWindow, wx){
 	var _module = {};
-	
+	function estudy(){
+		
+	}
+	estudy.prototype.init = function() {
+		  console.log("Success to load js !");
+		};
+	_module.estudy = new estudy();	
     return _module;
-})(base, gtnav, gj, uiForm, uiPopupWindow, wx);
+})(captcha, base, gtnav, gj, uiForm, uiPopupWindow, wx);
  
