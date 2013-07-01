5875a126d3dbfb1c48d2f82c94ded4a654c38784
pham tuan <phamtuanchip@gmail.com>
2013-07-02 03:13:29 +0700

    poc simple service , config and load from container

--- eportlet/src/main/java/org/estudy/ui/portlet/Eportlet.java
+++ eportlet/src/main/java/org/estudy/ui/portlet/Eportlet.java
@@ -4,6 +4,10 @@ package org.estudy.ui.portlet;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.core.UIPortletApplication;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
+import org.exoplatform.container.PortalContainer;
+import org.estudy.service.DataStorage;
+ import org.estudy.service.impl.JcrDataStorage;
+ 
 
 
 @ComponentConfig(
@@ -14,7 +18,9 @@ public class Eportlet extends UIPortletApplication
 { 
 	public Eportlet() throws Exception 
 	{
-		 
+		DataStorage dataService = (DataStorage) PortalContainer.getInstance().getComponentInstanceOfType(JcrDataStorage.class);
+		dataService.saveData();
+		dataService.getData();
 	}
 
 
