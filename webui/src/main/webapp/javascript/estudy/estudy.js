cdc60edf5fdb8797631c2a5086f911ca95327433
Tuan Pham <tuanp@exoplatform.com>
2013-10-28 16:11:15 +0700

    fix load js from resource

--- webui/src/main/webapp/javascript/estudy/estudy.js
+++ webui/src/main/webapp/javascript/estudy/estudy.js
@@ -1,12 +1,13 @@
-(function($){
+(function($,er){
 	var _module = {};
 	function estudy(){
 		
 	};
 	estudy.prototype.init = function() {
 		  console.log("Success to load js !");
+          er.init();
 	};
 	_module.estudy = new estudy();
     return _module;
-})($);
+})($, er);
  
