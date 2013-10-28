3d5c7e3c441a2a3743bf859557aa990d96871747
Tuan Pham <tuanp@exoplatform.com>
2013-10-28 16:31:08 +0700

    fix load player js

--- webui/src/main/webapp/javascript/estudy/estudy.js
+++ webui/src/main/webapp/javascript/estudy/estudy.js
@@ -1,13 +1,12 @@
-(function($,er){
+(function($){
 	var _module = {};
 	function estudy(){
 		
 	};
 	estudy.prototype.init = function() {
 		  console.log("Success to load js !");
-          er.init();
 	};
 	_module.estudy = new estudy();
     return _module;
-})($, er);
+})($);
  
