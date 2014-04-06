43ead7e67c99fa85be9c33388e94e4080737a398
phamtuanchip <phamtuanchip@gmail.com>
2014-04-07 01:52:32 +0700

    add media viewer

--- webui/src/main/java/org/estudy/ui/view/UIResourceViewer.java
+++ webui/src/main/java/org/estudy/ui/view/UIResourceViewer.java
@@ -3,10 +3,16 @@ package org.estudy.ui.view;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.jcr.RepositoryException;
+
+import org.estudy.learning.Util;
 import org.estudy.learning.model.Attachment;
 import org.estudy.learning.storage.DataStorage;
 import org.estudy.ui.portlet.EStudyPortlet;
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.portal.application.PortalRequestContext;
 import org.exoplatform.portal.webui.container.UIContainer;
+import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.event.Event;
@@ -20,6 +26,8 @@ import org.exoplatform.webui.event.EventListener;
 		)
 public class UIResourceViewer extends UIContainer {
 	Collection<Attachment> mediaList = new ArrayList<Attachment>()  ;
+	String selectedFullUrl_ = null;
+	String selectedWebdavUrl_ = null;
 	public Collection<Attachment> getMediaList(){
 		DataStorage service = EStudyPortlet.getDataService();
 		try {
@@ -28,6 +36,26 @@ public class UIResourceViewer extends UIContainer {
 			e.printStackTrace();
 		}
 		return mediaList;
+	}
+	public String getResourceUrl(Attachment att, String type, int w, int h) {
+		String id = att.getId().substring(att.getId().lastIndexOf(Util.SLASH) + 1);
+		return "/" + PortalContainer.getInstance().getRestContextName() +  "/estudy/api/media/" + id +"?type="+type+"&w="+w+"&h="+h;
+	}
+	public String getWebDavUrl(Attachment att) {
+		PortalRequestContext pContext = org.exoplatform.portal.webui.util.Util.getPortalRequestContext();
+		pContext.getRequest().getServerName() ;
+		pContext.getRequest().getProtocol();
+		pContext.getRequest().getServerPort();
+		RepositoryService rservice = (RepositoryService)PortalContainer.getInstance().getComponentInstanceOfType(RepositoryService.class);
+		try {
+			return  pContext.getRequest().getProtocol().split("/")[0].toLowerCase() + "://" +pContext.getRequest().getServerName()+ ":" + pContext.getRequest().getServerPort()+"/"+ PortalContainer.getCurrentPortalContainerName()+"/"+ PortalContainer.getInstance().getRestContextName() +"/jcr/"+ rservice.getCurrentRepository().getConfiguration().getName() + "/" + rservice.getCurrentRepository().getConfiguration().getDefaultWorkspaceName() + att.getDataPath();
+		} catch (RepositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+		return null;
 	}
 	static public class DownloadActionListener extends EventListener<UIResourceViewer> {
 		@Override
